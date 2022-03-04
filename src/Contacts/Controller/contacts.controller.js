a755fcb988d926882f7706b964eab595386ef608
unknown <iradukundacyuzuzo50@gmail.com>
2022-03-04 09:20:51 +0200

    Get one Message &Delete

--- src/Contacts/Controller/contacts.controller.js
+++ src/Contacts/Controller/contacts.controller.js
@@ -1,9 +1,20 @@
 const contacts = [];
 const getContacts = (req, res) => res.json({message: `all contacts Retrieved`, contacts});
+const getcontact = (req, res) => {
+    const {id} = req.params;
+    const contact = contacts.find((e) => e.id == id );
+    if(contact) return res.json({message: `contact ${contact.firstname} retrieved succesfully `, contact});
+    res.json({error: ` contact with id ${id} doesnt exist`}); 
+}
 const addContacts = (req, res) => {
     const contact = req.body;
     contact.id = contacts.length;
     contacts.push(contact);
     res.json({message: `Contact from ${contact.firstname} Succesfull Sent!`, contact});
 }
-export { getContacts, addContacts, contacts }
+const deleteContact = (req, res) => {
+    const {id} = req.params;
+    contacts.splice(id, 1);
+    res.json({message: `contact with id ${id} succesfull deleted!`});
+}
+export { getContacts, addContacts, getcontact, deleteContact, contacts }
--- src/Contacts/routes/contacts.routes.js
+++ src/Contacts/routes/contacts.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getContacts, addContacts} from '../Controller/contacts.controller.js';
+import { getContacts, addContacts,getcontact, deleteContact} from '../Controller/contacts.controller.js';
 import contactValidator from '../middleware/contacts.middleware.js'
 const router = express.Router();
 const contacts = [];
 
 router.use(express.json());
 router.get('/contacts', getContacts);
+router.get('/contacts/:id', getcontact);
 router.post('/contacts',contactValidator, addContacts);
+router.delete('/contacts/:id', deleteContact);
 
 export { router as default}
